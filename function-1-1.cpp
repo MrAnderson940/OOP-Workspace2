a9b1d10edf0829bd77f2ea4ad30a9e754e65a4a5
MrAnderson940 <mranderson@MSI>
2024-07-31 17:17:05 +0930

    1st 1-1

---
+++ function-1-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int sum_diagonal(int array[4][4]){
+    int sum;
+    sum = array[1][1] + array[2][2] + array[3][3] + array[4][4]; 
+
+    return sum;
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+extern int sum_diagonal(int array[4][4]);
+
+
+int main(){
+    int array[4][4] = {{1,2,3,4},{1,2,3,4},{1,2,3,4},{1,2,3,4}};
+    cout << sum_diagonal(array) << endl;
+
+    return 0;
+}
