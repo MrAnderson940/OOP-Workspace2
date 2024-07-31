1aec36ba443290b4effeb3b3e7dfdadab05686b1
MrAnderson940 <mranderson@MSI>
2024-07-31 17:19:02 +0930

    2nd 1-1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int sum_diagonal(int array[4][4]){
     int sum;
-    sum = array[1][1] + array[2][2] + array[3][3] + array[4][4]; 
+    sum = array[1][1] + array[2][2] + array[3][3] + array[0][0]; 
 
     return sum;
 }
