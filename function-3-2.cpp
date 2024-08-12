ef0a9b356844062a4bfa7e5120add32526e80f26
MrAnderson940 <mranderson@MSI>
2024-08-12 10:41:12 +0930

    2nd

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool equalsArray(int *numbers1,int *numbers2,int length){
-    for (int i=0; i<length; i++){
-        if (numbers1[i] != numbers2[i]){
-            return false;
-        }
-    }
-    return true;
-}
+
 
 
 int* reverseArray(int *numbers1,int length){
