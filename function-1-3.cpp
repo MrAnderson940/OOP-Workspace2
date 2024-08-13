29c85cacdf0c1620aeb1ca21e875eb872d85ddcb
DeskTop <mranderson@DESKTOP-N409QCA.localdomain>
2024-08-13 20:17:21 +0930

    2nd 1-3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -13,3 +13,14 @@ PersonList deepCopyPersonList(PersonList pl){
     }
     return L2;
 }
+
+PersonList createPersonList(int n){
+    PersonList L1;
+    L1.people = new Person[n];
+    for (int i=0;i<n;i++){
+        L1.people[i].age = 1;
+        L1.people[i].name = "Jane Doe";
+    }
+    L1.numPeople=n;
+    return L1;
+}
