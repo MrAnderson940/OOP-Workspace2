e6eea27413aad87b575c59c7a939b6f63e83e7d0
DeskTop <mranderson@DESKTOP-N409QCA.localdomain>
2024-08-13 20:01:05 +0930

    2nd 1-2

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -8,7 +8,7 @@ PersonList createPersonList(int n){
     L1.people = new Person[n];
     for (int i=0;i<n;i++){
         L1.people[i].age = 1;
-        L1.people[i].name = "John Doe";
+        L1.people[i].name = "Jane Doe";
     }
     L1.numPeople=n;
     return L1;
