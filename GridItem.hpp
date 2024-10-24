095e3fa03524c0a81879f7e47c9ac7117f1e1bf0
MrAnderson940 <mranderson@MSI>
2024-10-24 18:19:57 +1030

    4st

--- GridItem.hpp
+++ GridItem.hpp
@@ -43,7 +43,7 @@ int getGridHeight(){
 }
 
 int getActiveGridItemCount(){
-    return 5;
+    return itemCount;
 }
 
 ~GridItem(){}
--- Helper.hpp
+++ Helper.hpp
@@ -1,7 +1,10 @@
 #ifdef HELPER_HPP
 #define HELPER_HPP
 
+
 #pragma once
+#include <math.h>
+#include <iostrem> 
 
 class Helper{
 private:
@@ -11,8 +14,9 @@ public:
     / function to calculate the euclidean distance between two coordinates.
     double euclideanDistance(std::pair<int, int> item1, std::pair<int, int> item2){
         double n;
-        n = item2.getCordnates()-item1.getCordnates()
+        n = (item2.second-item1.second)^2 + (item2.first-item1.first)^2;
         n = n^(1/2);
+        return n;
     }
     ~Helper();
 };
