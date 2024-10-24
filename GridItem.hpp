998ebc71c65cb2311350a39df3148cb496e41c53
MrAnderson940 <mranderson@MSI>
2024-10-24 18:09:47 +1030

    4st

--- GridItem.hpp
+++ GridItem.hpp
@@ -14,42 +14,36 @@ GridItem(int lx, int ly, int w, int h): GridItem(){
     width=w;
     height=h;
     itemCount=0;
-};
+}
 GridItem(){
     x=0;
     y=0;
     width=1;
     height=1;
     itemCount=0;
-};
-std::pair<int,int> getCoordinates(){
-    std::pair<int,int> pair;
-    pair.first = x;
-    pair.second =y;
-    return pair;
-};
+}
 void setCoordinates(int lx, int ly){
     x=lx;
     y=ly;
-};
+}
 std::pair<int,int> getCoordinates(){
     std::pair<int,int> pair;
     pair.first = x;
     pair.second =y;
     return pair;
-};
+}
 
 int getGridWidth(){
     return width;
-};
+}
 
 int getGridHeight(){
     return height;
-};
+}
 
 int getActiveGridItemCount(){
     return itemCount;
-};
+}
 
 ~GridItem(){}
 };
